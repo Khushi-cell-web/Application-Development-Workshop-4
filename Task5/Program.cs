4495a79867b38a52e8793ddd6a3e4733d3dfb174
Khushi-cell-web <khushibohora692@gmail.com>
2025-11-19 22:44:17 +0545

    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter day: ");
+        string day = Console.ReadLine().ToLower();
+
+        if (day == "friday" || day == "saturday")
+            Console.WriteLine("It is: Weekend");
+        else
+            Console.WriteLine("It is: Weekday");
+
+        Book b1 = new Book("C# Basics", "John Doe", 500);
+        Book b2 = b1 with { title = "Advanced C#", price = 700 };
+
+        Console.WriteLine(b1);
+
+        var (title, author, price) = b2;
+        Console.WriteLine($"Deconstructed: {title}, {author}, {price}");
+    }
+}
