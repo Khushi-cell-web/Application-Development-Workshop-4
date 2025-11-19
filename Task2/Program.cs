aebac232765339d065db4e4746d0e562c3447707
Khushi-cell-web <khushibohora692@gmail.com>
2025-11-19 22:34:38 +0545

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Calculator calc = new Calculator();
+
+        calc.PrintWelcome();
+
+        int sum = calc.Add(5, 10);
+        int product = calc.Multiply(7);
+
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Multiplication: " + product);
+    }
+}
