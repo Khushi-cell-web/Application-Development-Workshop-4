001a34d020e98db1baf4dbd22bc836975262f7b7
Khushi-cell-web <khushibohora692@gmail.com>
2025-11-19 22:37:52 +0545

    Task3

---
+++ Task3/ParameterDemo.cs
@@ -0,0 +1,17 @@
+﻿public class ParameterDemo
+{
+    public void Increase(ref int number)
+    {
+        number += 10;
+    }
+
+    public void GetFullName(out string fullname)
+    {
+        fullname = "Khushi Bohora";
+    }
+
+    public int SumAll(params int[] numbers)
+    {
+        return numbers.Sum();
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        ParameterDemo pd = new ParameterDemo();
+
+        int value = 20;
+        pd.Increase(ref value);
+        Console.WriteLine("After Increase: " + value);
+
+        pd.GetFullName(out string fullName);
+        Console.WriteLine("Full Name: " + fullName);
+
+        int sum = pd.SumAll(1, 2, 3, 4, 5);
+        Console.WriteLine("Sum of All: " + sum);
+    }
+}
