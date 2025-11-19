09e22a44863d18e40c3e82510662b18f005946a5
Khushi-cell-web <khushibohora692@gmail.com>
2025-11-19 22:45:15 +0545

    Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1 = new Student();
+        s1.name = "Khushi";
+        s1.age = 20;
+        s1.course = "IT";
+
+        Student s2 = new Student();
+        s2.name = "Nirjala";
+        s2.age = 22;
+        s2.course = "Software Engineering";
+
+        Console.WriteLine("Student 1: " + s1.name + ", " + s1.age + ", " + s1.course);
+        Console.WriteLine("Student 2: " + s2.name + ", " + s2.age + ", " + s2.course);
+
+        Console.WriteLine("College: " + Student.college);
+    }
+}
---
+++ Task 1/Student.cs
@@ -0,0 +1,8 @@
+﻿public class Student
+{
+    public string name;
+    public int age;
+    public string course;
+
+    public static string college = "LFES ";
+}
