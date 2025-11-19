ac5f8fa3c4d41f06a5afa662803737274cc8ac9e
Khushi-cell-web <khushibohora692@gmail.com>
2025-11-19 22:40:41 +0545

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Player p1 = new Player();
+
+        Player p2 = new Player("Khushi", 10, 100);
+        Console.WriteLine($"Name: {p2.playerName}, Level: {p2.level}, Health: {p2.health}");
+    }
+}
